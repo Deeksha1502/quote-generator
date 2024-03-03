1fa3c0b40f22a0d06357b00b6e190ca70ccf1477
Deeksha <deekshad1502@gmail.com>
2024-03-04 00:09:19 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,63 +1,43 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Footer from "./components/footer/Footer";
 
 function App() {
-
-
-  const[quote, setQuote] = useState("");
-  const[author, setAuthor] = useState("");
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
   //https://api.quotable.io/random
 
-
-useEffect(()=>{
-  fetch("https://api.quotable.io/random")
-  .then(res=>res.json())
-  .then(
-(quote) =>
-  {
-    setQuote(quote.content);
-    setAuthor(quote.author);
-    console.log(quote);
-  }
-
-  )
-
-},
-  [])
-
-  let fetchNewQuote =() =>
-  {
+  useEffect(() => {
     fetch("https://api.quotable.io/random")
-    .then(res=>res.json())
-    .then(
-  (quote) =>
-    {
-      setQuote(quote.content);
-      setAuthor(quote.author);
-      console.log(quote);
-    }
-  
-    )
-  
-  }
+      .then((res) => res.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+        console.log(quote);
+      });
+  }, []);
 
-
-
-
-
+  let fetchNewQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+        console.log(quote);
+      });
+  };
 
   return (
     <div className="App">
-
-
-<div className='quote'>
-      <h2>{quote}</h2>
-      <small>~{author}~</small>
-
-    </div><br/>
-    <button className='btn' onClick={fetchNewQuote}>Generate new quote</button>
-
-    
+      <div className="quote">
+        <h2>{quote}</h2>
+        <small>~{author}~</small>
+      </div>
+      <br />
+      <button className="btn" onClick={fetchNewQuote}>
+        Generate new quote
+      </button>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h3>Have a good day!!</h3>
+    </div>
+  );
+}
+
+export default Footer;
